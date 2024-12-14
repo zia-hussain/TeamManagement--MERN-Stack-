564070c7704b4fe87cdd2f646cdd95794f471f59
zia-hussain <syedziashahgillani76@gmail.com>
2024-12-15 01:39:24 +0500

    done !!!

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,6 +1,9 @@
 // actionTypes.js
 export const FETCH_TEAMS = "FETCH_TEAMS";
+export const FETCH_TEAM_DETAILS = "FETCH_TEAM_DETAILS";
+export const UPDATE_TEAM_DETAILS = "UPDATE_TEAM_DETAILS";
 export const ADD_TEAM = "ADD_TEAM";
 export const DELETE_TEAM = "DELETE_TEAM";
+export const UPDATE_TEAM = "UPDATE_TEAM";
 export const ADD_MEMBER = "ADD_MEMBER";
 export const DELETE_MEMBER = "DELETE_MEMBER";
